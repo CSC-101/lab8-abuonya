23b206aa59858948255732c692b93f486cb012a7
annalisebuonya <abuonya@calpoly.edu>
2024-11-13 13:17:51 -0800

    task 2 - update 3

---
+++ .github/file.py
@@ -0,0 +1,7 @@
+# TASK 2
+    # Purpose of Function: takes a single command-line argument and reads every line in a text file consisting of two float values.
+    # For each line, convert the float values and print their sum.
+
+def read_all_file():
+    sample_file = open("test_file.txt", "r")
+
