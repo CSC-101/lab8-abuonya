9d634d01c926373be848a044634f3d1e3999306b
annalisebuonya <abuonya@calpoly.edu>
2024-11-13 13:25:31 -0800

    task 2 - update 4

--- .github/file.py
+++ .github/file.py
@@ -3,5 +3,9 @@
     # For each line, convert the float values and print their sum.
 
 def read_all_file():
-    sample_file = open("test_file.txt", "r")
+    with open("test_file", "r") as test_file:
+        for line in test_file:
+            contents =
+            print(line)
 
+read_all_file()
