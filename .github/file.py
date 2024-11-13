53736370687beb261d98e3737d70fd84c9dd6cc7
annalisebuonya <abuonya@calpoly.edu>
2024-11-13 13:55:49 -0800

    task 2 - update 10

--- .github/file.py
+++ .github/file.py
@@ -1,9 +1,12 @@
 # TASK 2
     # Purpose of Function: takes a single command-line argument and reads every line in a text file consisting of two float values.
     # For each line, convert the float values and print their sum.
+import sys
+
 
 def read_all_file():
-    with open("test_file", "r") as test_file:
+    command_line_input = sys.argv[1]
+    with open(command_line_input, "r") as test_file:
         tempr_list = []
         for line in test_file:
             split_contents = line.strip().split()
@@ -19,4 +22,6 @@ def read_all_file():
 
         return tempr_list
 
-read_all_file()
+if __name__ == "__main__":
+    test = read_all_file()
+    print("Completed processing.")
