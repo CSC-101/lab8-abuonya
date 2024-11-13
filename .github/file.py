e7a34efc611bddf5b8635b1f6e17bf403d67fa35
annalisebuonya <abuonya@calpoly.edu>
2024-11-13 13:47:56 -0800

    task 2 - update 9

--- .github/file.py
+++ .github/file.py
@@ -4,17 +4,19 @@
 
 def read_all_file():
     with open("test_file", "r") as test_file:
+        tempr_list = []
         for line in test_file:
             split_contents = line.strip().split()
             try:
-                print(split_contents)
                 x = float(split_contents[0])
                 y = float(split_contents[1])
                 total_sum = x + y
+                tempr_list.append(total_sum)
                 print(total_sum)
-                return total_sum
 
             except ValueError:
                 print("Error - an inputted value could not be processed.")
+
+        return tempr_list
 
 read_all_file()
