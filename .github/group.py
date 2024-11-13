a46a3440c9e41ac3aa637df45c10304faa563f6f
annalisebuonya <abuonya@calpoly.edu>
2024-11-13 13:05:39 -0800

    task 1 - update 3

--- .github/group.py
+++ .github/group.py
@@ -7,5 +7,8 @@ def groups_of_3(given_list: list[int]) -> list[list[int]]:
     tempr_list = []  # initialize an empty list to store groups of 3 in.
 
     for x in range(0,len(given_list), 3):
-        tempr_list.append(given_list[i:i+3])
+        tempr_list.append(given_list[x:x+3])
         print(tempr_list)
+
+    return tempr_list
+
